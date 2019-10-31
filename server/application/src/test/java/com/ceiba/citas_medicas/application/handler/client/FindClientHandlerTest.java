71372aad73ba95108054d2fae29fa0405d4a165f
Mateo Leal <mateolegi@gmail.com>
2019-10-31 16:32:43 -0500

    Fix test

--- server/application/src/test/java/com/ceiba/citas_medicas/application/handler/client/FindClientHandlerTest.java
+++ server/application/src/test/java/com/ceiba/citas_medicas/application/handler/client/FindClientHandlerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -18,10 +18,10 @@ class FindClientHandlerTest {
         // arrange
         var client = new Client(1L, "123", "John Doe");
         var service = mock(FindClientService.class);
-        doReturn(Optional.of(client)).when(service).execute(anyLong());
+        doReturn(Optional.of(client)).when(service).execute(anyString());
         var handler = new FindClientHandler(service);
 
         // act - assert
-        assertTrue(() -> handler.execute(1L).isPresent());
+        assertTrue(() -> handler.execute("").isPresent());
     }
 }
